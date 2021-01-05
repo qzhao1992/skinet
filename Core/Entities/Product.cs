c857dbe270c2f6cc556c3efb12281942e28bdc01
Qing <qzhao@uco.edu>
2021-01-04 22:29:30 -0600

    new migration

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,9 +1,16 @@
 using System;
 namespace Core.Entities
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public int Id { get; set; }
         public String Name { get; set; } 
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        public ProductType MyProperty { get; set; }
+        public int ProductTypeId { get; set; }
+        public ProductBrand ProductBrand { get; set; }
+        public int ProductBrandId { get; set; }
     }
+
 }
