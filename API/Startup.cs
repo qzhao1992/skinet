c8310a64f27c00ace838ce32e3c0c049efc128b1
agentq_kalidy <qing.zhao@kalidy.com>
2021-01-15 11:32:12 -0600

    finish up

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,11 @@ namespace API
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                });
+            });
             
         }
 
@@ -50,6 +55,7 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
             app.UseSwaggerDocumentation(); 
