1a77c274ff2856f66aae733ba64488ca3b808e54
agentq_kalidy <qing.zhao@kalidy.com>
2021-02-16 16:27:07 -0600

    add loading, improved ui

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { PagerComponent } from './components/pager/pager.component';
 
 
@@ -9,12 +10,14 @@ import { PagerComponent } from './components/pager/pager.component';
   declarations: [PagingHeaderComponent, PagerComponent],
   imports: [
     CommonModule,
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    CarouselModule.forRoot()
   ],
   exports: [
     PaginationModule,
     PagingHeaderComponent,
     PagerComponent,
+    CarouselModule
   ]
 })
 export class SharedModule { }
